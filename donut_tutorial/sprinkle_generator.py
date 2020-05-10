d0c656cd7a030c1318fe54d77c411b007ef45106
Egan McClave <eganmcclave@yahoo.com>
2020-05-10 18:10:43 -0400

    Create sprinkle generator and polish donut render
    This commit includes the creation of `sprinkle_generator.py` which will
    be used to generate random variations of sprinkles. It utilizes many of
    the similar functions for `donut_generator.py`.
    
    Created the following files:
    
    1) "sprinkle_generator.py"
    
      This script randomly creates sprinkle mesh objects of varying size (in
      both radius and height).
    
    Altered the following files:
    
    1) "donut.blend"
    
      This .blend file now includes some hand-sculpting on top of a
      procedurally generaed donut. It also includes a more polished scene
      for rendering an image.
    
    2) "donute_generator.py"
    
      This .py file has some minor updates to the custom functions.

--- donut_tutorial/sprinkle_generator.py
+++ donut_tutorial/sprinkle_generator.py
@@ -6,8 +6,6 @@ from random import random, sample, shuffle, uniform
 import bpy
 import bmesh
 
-# Local library imports
-
 
 ######################
 # Reusable functions #
@@ -28,7 +26,7 @@ def rename_last_mesh(new_name):
 
 def select_vertices(prop=0.03, vertex_fxn_crit=lambda v: True):
     
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
 
@@ -52,7 +50,7 @@ def select_vertices(prop=0.03, vertex_fxn_crit=lambda v: True):
 
 def select_faces(vertex_fxn_crit=lambda v: v.co.z > 0):
     
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
         
@@ -76,7 +74,7 @@ def select_faces(vertex_fxn_crit=lambda v: v.co.z > 0):
 
 def select_border_edges(prop=0.25):
     
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
         
@@ -97,9 +95,9 @@ def select_border_edges(prop=0.25):
     
     
 def shift_vertices_along_axes(n_axes=1, shift_min=-0.008, shift_max=0.009, p_size=0.0355841,
-    force_coords=(False, False, False)):
+    force_coords=(False, False, False), exit_edit_mode=True):
 
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
         
@@ -134,16 +132,19 @@ def shift_vertices_along_axes(n_axes=1, shift_min=-0.008, shift_max=0.009, p_siz
         use_proportional_connected=False, use_proportional_projected=False
     )
 
-    # Deselect all components of mesh object
-    bpy.ops.mesh.select_all(action='DESELECT')
-        
-    # Switch back into edit mode
-    bpy.ops.object.editmode_toggle()
-
-
-def shift_duplicated_faces(shift_dims=[0, 0, 0]):
     
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Exit `Edit` mode
+    if exit_edit_mode:
+        # Deselect all components of mesh object
+        bpy.ops.mesh.select_all(action='DESELECT')
+    
+        # Switch back into `Object` mode
+        bpy.ops.object.editmode_toggle()
+
+
+def shift_duplicated_faces(shift_dims=[0, 0, 0], exit_edit_mode=True):
+    
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
     
@@ -165,17 +166,19 @@ def shift_duplicated_faces(shift_dims=[0, 0, 0]):
     # Initialize the duplicated mesh as a separte mesh
     bpy.ops.mesh.separate(type='SELECTED')
     
-    # Deselect all components of mesh object
-    bpy.ops.mesh.select_all(action='DESELECT')
-
-    # Switch back into edit mode
-    bpy.ops.object.editmode_toggle()
+    # Exit `Edit` mode
+    if exit_edit_mode:
+        # Deselect all components of mesh object
+        bpy.ops.mesh.select_all(action='DESELECT')
+    
+        # Switch back into `Object` mode
+        bpy.ops.object.editmode_toggle()
 
 
 def extrude_edges(n_axes=1, shift_min=-0.002, shift_max=0.002, p_size=0.0200863,
-    force_coords=(False, False, False)):
+    force_coords=(False, False, False), exit_edit_mode=True):
         
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
 
@@ -216,124 +219,74 @@ def extrude_edges(n_axes=1, shift_min=-0.002, shift_max=0.002, p_size=0.0200863,
         }
     )
 
-    # Deselect all components of mesh object
-    bpy.ops.mesh.select_all(action='DESELECT')
+    # Exit `Edit` mode
+    if exit_edit_mode:
+        # Deselect all components of mesh object
+        bpy.ops.mesh.select_all(action='DESELECT')
     
-    # Switch back into edit mode
-    bpy.ops.object.editmode_toggle()
+        # Switch back into `Object` mode
+        bpy.ops.object.editmode_toggle()
         
 
-def resize(val=0.2, p_size=0.0137192):
+def resize(n_axes=1, shift_min=0.0, shift_max=1, p_size=0.0200863,
+    force_coords=(False, False, False), exit_edit_mode=True):
     
-    # Checks if in `Edit` mode and toggle to `Object` mode
+    # Checks if in `Object` mode and toggle to `Edit` mode
     if 'EDIT' not in bpy.context.mode:
         bpy.ops.object.editmode_toggle()
         
+    # Uniformly select random values between min/max provided then shuffle them
+    values = [uniform(shift_min, shift_max) for _ in range(n_axes)]
+    ones = [1 for _ in range(3-n_axes)]
+
+    if force_coords[0]:
+        shift_values = values[1:] + ones
+        shuffle(shift_values)
+        shift_values.insert(0, values[0])
+    elif force_coords[1]:
+        shift_values = values[1:] + ones
+        shuffle(shift_values)
+        shift_values.insert(1, values[0])
+    elif force_coords[2]:
+        shift_values = values[1:] + ones
+        shuffle(shift_values)
+        shift_values.insert(2, values[0])
+    else:
+        shift_values = values + ones
+        shuffle(shift_values)
+        
+    # Limit movement to the appropriate axes
+    constraint_axis = [bool(value) for value in shift_values]
+        
     bpy.ops.transform.resize(
-        value=(val, val, val), orient_type='GLOBAL', 
+        value=shift_values, orient_type='GLOBAL', 
         orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', 
-        mirror=True, use_proportional_edit=True, proportional_edit_falloff='SHARP', 
-        proportional_size=p_size, use_proportional_connected=False, 
-        use_proportional_projected=False
+        constraint_axis=(False, False, True), mirror=True, use_proportional_edit=False, 
+        proportional_edit_falloff='SMOOTH', proportional_size=p_size, 
+        use_proportional_connected=False, use_proportional_projected=False
     )
+        
+    # Exit `Edit` mode
+    if exit_edit_mode:
+        # Deselect all components of mesh object
+        bpy.ops.mesh.select_all(action='DESELECT')
     
-    # Deselect all components of mesh object
-    bpy.ops.mesh.select_all(action='DESELECT')
-    
-    # Switch back into edit mode
-    bpy.ops.object.editmode_toggle()
+        # Switch back into `Object` mode
+        bpy.ops.object.editmode_toggle()
 
 
-###############
-# Set-up Code #
-###############
+##########################
+# Sprinkle Creation Code #
+##########################
 
-# Enables Proportional Editing mode
-bpy.context.scene.tool_settings.use_proportional_edit = True
-
-# Checks if in `Edit` mode and toggle to `Object` mode
-if 'EDIT' in bpy.context.mode:
-    bpy.ops.object.editmode_toggle()
-
-# Deletes all existing objects in scene
-bpy.ops.object.delete(use_global=False)
-
-# Removes their mesh data as well
-for k in bpy.data.meshes.keys():
-    mesh = bpy.data.meshes[k]
-    bpy.data.meshes.remove(mesh)
-
-#######################
-# Donut Creation Code #
-#######################
-
-# Converts units to Metric Unit System
-bpy.context.scene.unit_settings.system = 'METRIC'
-
-# Converts units to Imperial Unit System
-#bpy.context.scene.unit_settings.system = 'IMPERIAL'
-
-# Add torus mesh object and name it `Donut`
-bpy.ops.mesh.primitive_torus_add(
-    align='WORLD', major_segments=28, minor_segments=12, major_radius=0.0325, minor_radius=0.0151
+rad_val = uniform(0.0005, 0.0015)
+bpy.ops.mesh.primitive_uv_sphere_add(
+    segments=12, ring_count=6, radius=rad_val, enter_editmode=False, location=(0, 0, 0)
 )
-rename_last_mesh('Donut')
 
-# Add Subdivision Surface modifier to increase subdivisions
-bpy.ops.object.modifier_add(type='SUBSURF')
-bpy.context.object.modifiers["Subdivision"].levels = 2
+select_faces(lambda v: v.co.z >= -0.0001)
+extrude_edges(shift_min=0.002, shift_max=0.005, force_coords=(False, False, True), exit_edit_mode=False)
+resize(n_axes=1, shift_min=0.1, shift_max=0.3, p_size=1, force_coords=(False, False, True))
+select_faces(lambda v: v.co.z < 0.0001)
+resize(n_axes=1, shift_min=0.1, shift_max=0.3, p_size=1, force_coords=(False, False, True))
 
-# Select and alter vertices to give imperfections
-select_vertices()
-shift_vertices_along_axes(2, -0.003, 0.003, 0.017, force_coords=(True, False, True))
-
-select_vertices()
-shift_vertices_along_axes(1, -0.003, 0.0015, 0.02, force_coords=(False, False, True))
-
-# Select and duplicated faces on top of object to create glaze
-select_faces()
-shift_duplicated_faces()
-rename_last_mesh('Icing')
-
-# Change the active object to `Icing` and apply the Solidify modifier
-bpy.context.view_layer.objects.active = bpy.data.objects['Icing']
-bpy.ops.object.modifier_add(type='SOLIDIFY')
-
-# Change placement of Solidify modifier and the thickness & offset values
-bpy.ops.object.modifier_move_up(modifier="Solidify")
-bpy.context.object.modifiers["Solidify"].thickness = 0.0015
-bpy.context.object.modifiers["Solidify"].offset = 1
-bpy.context.object.modifiers["Solidify"].edge_crease_inner = 1
-
-# Add subdivide modifier to `Icing`
-select_faces()
-bpy.ops.mesh.subdivide(smoothness=1)
-bpy.ops.mesh.select_all(action='DESELECT')
-
-# Change snap element settings
-bpy.context.scene.tool_settings.use_snap = True
-bpy.context.scene.tool_settings.snap_elements = {'FACE'}
-bpy.context.scene.tool_settings.use_snap_project = True
-
-# Change proportional edit settings
-bpy.context.scene.tool_settings.proportional_edit_falloff = 'SHARP'
-
-# Repeatedly grab border edges and extrude them at different ranges
-select_border_edges(prop=0.5)
-shift_vertices_along_axes(1, -0.003, 0, 0.010, (False, False, True))
-
-select_border_edges(prop=0.01)
-extrude_edges(1, -0.005, 0, force_coords=(False, False, True))
-resize(val=0.4)
-
-#select_border_edges(prop=0.05)
-#extrude_edges(1, -0.003, 0, p_size=0.08, force_coords=(False, False, True))
-#resize(val=0.8)
-
-# Change proportional edit settings back to normal
-bpy.context.scene.tool_settings.proportional_edit_falloff = 'SMOOTH'
-
-# Change snap element settings back to normal
-bpy.context.scene.tool_settings.use_snap_project = False
-bpy.context.scene.tool_settings.snap_elements = {'INCREMENT'}
-bpy.context.scene.tool_settings.use_snap = False
